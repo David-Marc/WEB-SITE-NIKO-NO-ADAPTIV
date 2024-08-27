981741ead6d29dc491f35146d8496d0ecec0baeb
David-Marc <jaxaia321@gmail.com>
2024-08-27 10:02:08 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,11 @@
+export const Blok = [
+    { 
+        image:'./src/assets/Main/flamingo.svg',
+        image:'./src/assets/Main/nlo-1.svg',
+        image:'./src/assets/Main/nlo-2.svg',
+        image:'./src/assets/Main/hero.svg'
+    },
+
+]
+
+export const flamingo = './src/assets/Main/hero.svg';
